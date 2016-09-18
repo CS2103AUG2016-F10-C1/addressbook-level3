b71b0882203ba057f5ada0c94eb579c4c051a345
Wei Kang <weikangchia@gmail.com>
2016-09-18 12:16:59 +0800

    Add tag to CommandResult

--- src/seedu/addressbook/commands/Command.java
+++ src/seedu/addressbook/commands/Command.java
@@ -3,6 +3,7 @@ package seedu.addressbook.commands;
 import seedu.addressbook.common.Messages;
 import seedu.addressbook.data.AddressBook;
 import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.tag.ReadOnlyTag;
 
 import java.util.List;
 
@@ -14,6 +15,7 @@ import static seedu.addressbook.ui.Gui.DISPLAYED_INDEX_OFFSET;
 public abstract class Command {
     protected AddressBook addressBook;
     protected List<? extends ReadOnlyPerson> relevantPersons;
+    protected List<? extends ReadOnlyTag> relevantTags;
     private int targetIndex = -1;
 
     /**
@@ -35,6 +37,16 @@ public abstract class Command {
     public static String getMessageForPersonListShownSummary(List<? extends ReadOnlyPerson> personsDisplayed) {
         return String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, personsDisplayed.size());
     }
+    
+    /**
+     * Constructs a feedback message to summarise an operation that displayed a listing of tags.
+     *
+     * @param tagsDisplayed used to generate summary
+     * @return summary message for tags displayed
+     */
+    public static String getMessageForTagListShownSummary(List<? extends ReadOnlyTag> tagsDisplayed) {
+        return String.format(Messages.MESSAGE_TAGS_LISTED_OVERVIEW, tagsDisplayed.size());
+    }
 
     /**
      * Executes the command and returns the result.
@@ -44,18 +56,29 @@ public abstract class Command {
     /**
      * Supplies the data the command will operate on.
      */
-    public void setData(AddressBook addressBook, List<? extends ReadOnlyPerson> relevantPersons) {
+    public void setData(AddressBook addressBook, List<? extends ReadOnlyPerson> relevantPersons,
+            List<? extends ReadOnlyTag> relevantTags) {
         this.addressBook = addressBook;
         this.relevantPersons = relevantPersons;
+        this.relevantTags = relevantTags;
     }
 
     /**
-     * Extracts the the target person in the last shown list from the given arguments.
+     * Extracts the target person in the last shown list from the given arguments.
      *
      * @throws IndexOutOfBoundsException if the target index is out of bounds of the last viewed listing
      */
     protected ReadOnlyPerson getTargetPerson() throws IndexOutOfBoundsException {
         return relevantPersons.get(getTargetIndex() - DISPLAYED_INDEX_OFFSET);
+    }
+    
+    /**
+     * Extracts the target tag in the last shown list from the given arguments.
+     *
+     * @throws IndexOutOfBoundsException if the target index is out of bounds of the last viewed listing
+     */
+    protected ReadOnlyTag getTargetTag() throws IndexOutOfBoundsException {
+        return relevantTags.get(getTargetIndex() - DISPLAYED_INDEX_OFFSET);
     }
 
     public int getTargetIndex() {
--- src/seedu/addressbook/commands/CommandResult.java
+++ src/seedu/addressbook/commands/CommandResult.java
@@ -1,6 +1,7 @@
 package seedu.addressbook.commands;
 
 import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.tag.ReadOnlyTag;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,15 +16,21 @@ public class CommandResult {
 
     /** The list of persons that was produced by the command */
     private final List<? extends ReadOnlyPerson> relevantPersons;
+    
+    /** The list of tags that was produced by the command */
+    private final List<? extends ReadOnlyTag> relevantTags;
 
     public CommandResult(String feedbackToUser) {
         this.feedbackToUser = feedbackToUser;
         relevantPersons = null;
+        relevantTags = null;
     }
 
-    public CommandResult(String feedbackToUser, List<? extends ReadOnlyPerson> relevantPersons) {
+    public CommandResult(String feedbackToUser, List<? extends ReadOnlyPerson> relevantPersons,
+            List<? extends ReadOnlyTag> relevantTags) {
         this.feedbackToUser = feedbackToUser;
         this.relevantPersons = relevantPersons;
+        this.relevantTags = relevantTags;
     }
 
     /**
@@ -33,4 +40,10 @@ public class CommandResult {
         return Optional.ofNullable(relevantPersons);
     }
 
+    /**
+     * Returns list of tags relevant to the command command result, if any.
+     */
+    public Optional<List<? extends ReadOnlyTag>> getRelevantTags() {
+        return Optional.ofNullable(relevantTags);
+    }
 }
