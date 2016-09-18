67fec45553d35a3fad3aa535d313144681dae190
johnervan <johnervan@u.nus.edu>
2016-09-18 15:06:36 +0800

    edit FindCommand.java

--- src/seedu/addressbook/commands/FindCommand.java
+++ src/seedu/addressbook/commands/FindCommand.java
@@ -5,17 +5,17 @@ import seedu.addressbook.data.person.ReadOnlyPerson;
 import java.util.*;
 
 /**
- * Finds and lists all persons in address book whose name contains any of the
- * argument keywords. Keyword matching is case sensitive.
+ * Finds and lists all persons in address book whose name contains any of the argument keywords.
+ * Keyword matching is case sensitive.
  */
 public class FindCommand extends Command {
 
     public static final String COMMAND_WORD = "find";
 
-    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Finds all persons whose contact details "
-            + "contain any of the specified keywords (case-insensitive) and displays them as a list with "
-            + "index numbers.\n" + "Parameters: KEYWORD [MORE_KEYWORDS]...\n" + "Example: " + COMMAND_WORD 
-            + " alice bob 96342343 john@gmail.com blk 253 Clementi Ave 6 #12-21";
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ":\n" + "Finds all persons whose names contain any of "
+            + "the specified keywords (case-insensitive) and displays them as a list with index numbers.\n\t"
+            + "Parameters: KEYWORD [MORE_KEYWORDS]...\n\t"
+            + "Example: " + COMMAND_WORD + " alice bob charlie";
 
     private final Set<String> keywords;
 
@@ -32,29 +32,27 @@ public class FindCommand extends Command {
 
     @Override
     public CommandResult execute() {
-        final List<ReadOnlyPerson> personsFound = getPersonsWithDataContainingAnyKeyword(keywords);
+        final List<ReadOnlyPerson> personsFound = getPersonsWithNameContainingAnyKeyword(keywords);
         return new CommandResult(getMessageForPersonListShownSummary(personsFound), personsFound);
     }
 
     /**
-     * Retrieve all persons in the address book whose names contain some of the
-     * specified keywords.
+     * Retrieve all persons in the address book whose names contain some of the specified keywords.
      *
-     * @param keywords
-     *            for searching
+     * @param keywords for searching
      * @return list of persons found
      */
-    private List<ReadOnlyPerson> getPersonsWithDataContainingAnyKeyword(Set<String> keywords) {
+    private List<ReadOnlyPerson> getPersonsWithNameContainingAnyKeyword(Set<String> keywords) {
         final List<ReadOnlyPerson> matchedPersons = new ArrayList<>();
         for (ReadOnlyPerson person : addressBook.getAllPersons()) {
-            Set<String> dataInPerson = fillPersonDataSet(person);
+            final Set<String> dataInPerson = fillPersonDataSet(person);
             if (!Collections.disjoint(changeToUpperCase(dataInPerson), changeToUpperCase(keywords))) {
                 matchedPersons.add(person);
             }
         }
         return matchedPersons;
     }
-
+    
     /**
      * Consolidate all the non-private data of a person into a set
      * 
@@ -78,7 +76,7 @@ public class FindCommand extends Command {
 
         return dataInPerson;
     }
-
+    
     /**
      * Changes all words in wordsInName and keywords to Upper Case
      * 
