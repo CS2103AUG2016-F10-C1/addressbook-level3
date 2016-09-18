b92cda0ee45c98a38191b01acd87be6bff04ee20
jaeoheeail <joelfooet@gmail.com>
2016-09-19 00:24:10 +0800

    Add new prefixes for add/remove tag

--- src/seedu/addressbook/common/Prefixes.java
+++ src/seedu/addressbook/common/Prefixes.java
@@ -1,15 +1,24 @@
 package seedu.addressbook.common;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Container for user input prefixes.
  */
 public class Prefixes {
 
-    public static final int LENGTH = 2;
+    public static final int LENGTH_DEFAULT = 2;
+    public static final int LENGTH_TAG = 3;
     
     public static final String NAME = "n/";
     public static final String PHONE = "p/";
     public static final String EMAIL = "e/";
     public static final String ADDRESS = "a/";
+    public static final String ADDTAG = "ta/";
+    public static final String REMOVETAG = "tr/";
+    
+    public static final List<String> DEFAULT_PREFIXES = Arrays.asList(NAME, PHONE, EMAIL, ADDRESS);
+    public static final List<String> TAG_PREFIXES = Arrays.asList(ADDTAG, REMOVETAG);
     
 }
