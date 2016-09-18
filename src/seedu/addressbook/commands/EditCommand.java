d3c782b491e67d733f08689c842799b41ca716e8
jaeoheeail <joelfooet@gmail.com>
2016-09-18 13:53:20 +0800

    Add EditCommand.java

---
+++ src/seedu/addressbook/commands/EditCommand.java
@@ -0,0 +1,47 @@
+package seedu.addressbook.commands;
+
+import seedu.addressbook.common.Messages;
+import seedu.addressbook.data.exception.IllegalValueException;
+import seedu.addressbook.data.person.ReadOnlyPerson;
+import seedu.addressbook.data.person.UniquePersonList.PersonNotFoundException;
+
+
+/**
+ * Edit a person identified using it's last displayed index from the address book.
+ */
+public class EditCommand extends Command {
+
+    public static final String COMMAND_WORD = "edit";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Edits any details of a person identified by the index number used in the last person listing.\n"
+            + "Parameters: INDEX n/NAME p/PHONE e/EMAIL a/ADDRESS\n"
+            + "Example: " + COMMAND_WORD + " 1 n/John Doe p/98765432 e/johnd@gmail.com a/311, Clementi Ave 2, #02-25";
+
+    public static final String MESSAGE_EDIT_PERSON_SUCCESS = "Edited Person: %1$s";
+
+    private String[] argsToEdit;
+
+
+    public EditCommand(int targetVisibleIndex, String[] argsToEdit) {
+        super(targetVisibleIndex);
+        this.argsToEdit = argsToEdit;
+    }
+
+
+    @Override
+    public CommandResult execute() {
+        try {
+            final ReadOnlyPerson target = getTargetPerson();
+            final ReadOnlyPerson editedTarget = addressBook.editPerson(target, argsToEdit);
+            return new CommandResult(String.format(MESSAGE_EDIT_PERSON_SUCCESS, editedTarget));
+        } catch (IndexOutOfBoundsException ie) {
+            return new CommandResult(Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+        } catch (PersonNotFoundException pnfe) {
+            return new CommandResult(Messages.MESSAGE_PERSON_NOT_IN_ADDRESSBOOK + Messages.MESSAGE_EXECUTE_LIST);
+        } catch (IllegalValueException ive) {
+            return new CommandResult(ive.getMessage());
+        }
+    }
+
+}
