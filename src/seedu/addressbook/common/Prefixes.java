aa6a9f005e1e8bc27f86797bd65619276ce8237c
jaeoheeail <joelfooet@gmail.com>
2016-09-18 13:51:10 +0800

    Add Prefixes.java to contain user input prefixes

---
+++ src/seedu/addressbook/common/Prefixes.java
@@ -0,0 +1,15 @@
+package seedu.addressbook.common;
+
+/**
+ * Container for user input prefixes.
+ */
+public class Prefixes {
+
+    public static final int LENGTH = 2;
+    
+    public static final String NAME = "n/";
+    public static final String PHONE = "p/";
+    public static final String EMAIL = "e/";
+    public static final String ADDRESS = "a/";
+    
+}
